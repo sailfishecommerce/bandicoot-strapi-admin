7e09aadf809d5d9d13fb3ba44c96cb60967e7c5f
BisiDavi <oludavidconnect@gmail.com>
2022-02-01 09:34:41 +0100

    strapi database updated

--- config/database.js
+++ config/database.js
@@ -1,23 +1,12 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        user: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      debug: false,
       options: {
         useNullAsDefault: true,
       },

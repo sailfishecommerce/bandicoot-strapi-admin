5265beef45eb372b29ddb9edb68b1482a7a9cf0b
BisiDavi <oludavidconnect@gmail.com>
2022-02-01 01:28:52 +0100

    page updated

--- config/plugins.js
+++ config/plugins.js
@@ -12,3 +12,5 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+// postgres://axbinxtsipthxf:841761ac57c1b9f77a93f0ab47eb8b041edad0b071fc834713aae94880745e98@ec2-34-205-209-14.compute-1.amazonaws.com:5432/d2i0ifsn1ocn9a

814b57046191410722f51c643b5fdf469ba5a352
BisiDavi <oludavidconnect@gmail.com>
2022-02-01 09:29:08 +0100

    strapi database updated

--- config/database.js
+++ config/database.js
@@ -3,18 +3,24 @@ const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   defaultConnection: "default",
-  connection: {
-    client: "postgres",
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      debug: false,
+      options: {
+        useNullAsDefault: true,
       },
     },
-    debug: false,
   },
 });
